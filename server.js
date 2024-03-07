1af6d7027e7bd956aa7f22886e8cf7295acb3748
serroukh <serroukh94@gmail.com>
2024-03-07 10:23:34 +0100

    modif port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 5122; 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
